9dbdb8a0b329f1a8ea59ec59c1403b29bba06882
Gilnei Barreto <gilneibarretos@gmail.com>
2024-05-09 00:47:03 -0300

    final

--- src/br/com/dio/desafio/dominio/Curso.java
+++ src/br/com/dio/desafio/dominio/Curso.java
@@ -1,43 +1,32 @@
 package br.com.dio.desafio.dominio;
 
-public class Curso {
-    private String titulo;
-    private String descricao;
-    private int cargaHraria;
+public class Curso extends Conteudo{
+
+    private int cargaHoraria;
 
     public Curso() {
     }
 
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
+    @Override
+    public double calcularXP() {
+        return XP_PADRAO * cargaHoraria;
     }
 
     public int getCargaHraria() {
-        return cargaHraria;
+        return cargaHoraria;
     }
 
-    public void setCargaHraria(int cargaHraria) {
-        this.cargaHraria = cargaHraria;
+    public void setCargaHoraria(int cargaHraria) {
+        this.cargaHoraria = cargaHraria;
     }
 
     @Override
     public String toString() {
         return "Curso{" +
-                "titulo='" + titulo + '\'' +
-                ", descricao='" + descricao + '\'' +
-                ", cargaHraria=" + cargaHraria +
+                "titulo='" + super.getTitulo() + '\'' +
+                ", descricao='" + super.getDescricao() + '\'' +
+                ", cargaHraria=" + getCargaHraria() +
                 '}';
     }
+
 }
